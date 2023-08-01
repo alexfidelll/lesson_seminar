2b960ba94534ea03532178963715722fb89a2976
fidel.alex <alekseifidel@gmail.com>
2023-08-02 00:33:11 +0500

    lesson3-4

--- 4_lesson/Program.cs
+++ 4_lesson/Program.cs
@@ -1,20 +1,17 @@
 ﻿Console.WriteLine("Введите целое число");
 int input1 = Convert.ToInt32(Console.ReadLine());
+int input2 = - input1;
 if (input1 > 0)
 {
-int input2 = - input1;
 while (input2 <= input1)
 {
     Console.WriteLine(input2);
     input2 ++ ;
 }}
-
-// input = -3 
 else
 {
-int input3 = - input1;
-while (input3 >= input1) // -3 >= 3 
+    while (input1 <= input2)
 {
-    Console.WriteLine(input1); // -3 
+    Console.WriteLine(input1);
     input1 ++ ;
-}}
+}}q
