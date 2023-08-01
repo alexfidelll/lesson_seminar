c7cb9a16728923f0a53a68543838844e9bb48d8c
fidel.alex <alekseifidel@gmail.com>
2023-08-02 00:28:46 +0500

    lesson3-4

---
+++ 5_lesson/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число");
+int input1 = Convert.ToInt32(Console.ReadLine());
+if (input1 > 100 && input1 < 999)
+{
+int input2 = input1 % 100; 
+int input3 = input2 % 10;
+Console.WriteLine(input3);
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
